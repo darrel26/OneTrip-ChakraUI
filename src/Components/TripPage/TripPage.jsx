44d1a5fa9eeebdf10a8db3354a1efcf9d5e5fe26
Ervan Jovian <ervanjovian@pm.me>
2022-12-26 20:14:11 +0700

    Fix route error

--- src/Components/TripPage/TripPage.jsx
+++ src/Components/TripPage/TripPage.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useJsApiLoader, GoogleMap, MarkerF } from '@react-google-maps/api';
+import { useJsApiLoader, GoogleMap, MarkerF, DirectionsRenderer } from '@react-google-maps/api';
 import { Container, HStack } from '@chakra-ui/react';
 import EditTripSection from './Section/EditTrip/EditTripSection';
 import { useSelector, useDispatch } from 'react-redux';
 
 let libraries = ['places'];
 let placeServices;
+let directionsRenderer
 
 const center = {
   lat: -6.914864,
@@ -15,6 +16,7 @@ const center = {
 export default function TripPage() {
   const [recommendation, setRecommendation] = useState([]);
   const [placeData, setPlaceData] = useState([]);
+  const [directionResponse,setDirectionResponse] = useState([])
 
   const addPlaces = (placeDetail) => {
     if (placeData.length !== 0) {
@@ -43,6 +45,25 @@ export default function TripPage() {
 
   const onLoad = (map) => {
     placeServices = new google.maps.places.PlacesService(map);
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map)
+  };
+
+  const calculateRoute = async () =>{
+      //eslint-disable-next-line no-undef
+    const directionService = new google.maps.DirectionsService();
+    const result = await directionService.route({
+      origin: {lat: -6.914864,lng: 107.608238 },
+      destination:  {lat: -6.714864,lng: 106.608238 },
+      //eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionResponse(result)
+
+  }
+
+  const clearRouter = () => {
+    setDirectionResponse(null);
   };
 
   useEffect(() => {
@@ -53,6 +74,10 @@ export default function TripPage() {
       });
     }
   }, [placeData]);
+
+  useEffect(() => {
+    console.log(directionResponse)
+  },[directionResponse])
 
   /* BUDGETTING */
 
@@ -83,6 +108,8 @@ export default function TripPage() {
 
   return (
     <Container maxW="100vw" p={0}>
+      <button onClick={calculateRoute}>Show Route</button>
+      <button onClick={clearRouter}>Clear Route</button>
       {isLoaded ? (
         <HStack p={0} spacing={0}>
           <EditTripSection
@@ -100,6 +127,9 @@ export default function TripPage() {
               height: '100vh',
               width: '60vw',
             }}
+            options={{
+            streetViewControl: false,
+            }}
             zoom={14}
             center={center}
             onLoad={(map) => onLoad(map)}
@@ -113,6 +143,9 @@ export default function TripPage() {
                 }}
               />
             ))}
+            {directionResponse && (
+            <DirectionsRenderer directions={directionResponse} />
+          )}
           </GoogleMap>
         </HStack>
       ) : (

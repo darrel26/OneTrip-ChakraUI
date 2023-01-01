9f6899ce57b5c7e80431ac0c00e34dbb75dbe7ea
Noid <dionisiusgunadi@outlook.com>
2023-01-01 22:35:31 +0700

    fix container maxW in AddTrip page

--- src/Components/AddTrip/AddTrip.jsx
+++ src/Components/AddTrip/AddTrip.jsx
@@ -1,29 +1,36 @@
-import { Container, Flex, Heading, HStack, Input, InputGroup, VStack, Button } from '@chakra-ui/react'
-import { Autocomplete } from '@react-google-maps/api'
-import React from 'react'
-import Navbar from '../HomePage/Section/Hero/components/Navbar'
-import InputAddTrip from './components/InputAddTrip'
-import RecommendButton from './components/RecommendButton'
+import { Container, Heading, VStack, Button } from '@chakra-ui/react';
+import React from 'react';
+import Navbar from '../HomePage/Section/Hero/components/Navbar';
+import InputAddTrip from './components/InputAddTrip';
+import RecommendButton from './components/RecommendButton';
 
 const AddTrip = () => {
   return (
     <Container
-      maxW="100vw"
+      maxW="container.xl"
       display="flex"
       justifyContent="center"
       className="Hero-Container"
     >
-      <VStack w="100vw"  display="flex" >
-        <Navbar/>
+      <VStack w="100vw" display="flex">
+        <Navbar />
         <VStack h="80vh" justifyContent="center" gap="1rem">
           <Heading color="teal">Add New Trip</Heading>
-          <InputAddTrip/>
-          <Button w="200px" h="50px" fontSize="22px" fontWeight="bold" colorScheme="teal">CREATE</Button>
-          <RecommendButton/>
+          <InputAddTrip />
+          <Button
+            w="200px"
+            h="50px"
+            fontSize="22px"
+            fontWeight="bold"
+            colorScheme="teal"
+          >
+            CREATE
+          </Button>
+          <RecommendButton />
         </VStack>
       </VStack>
     </Container>
-  )
-}
+  );
+};
 
-export default AddTrip
+export default AddTrip;
